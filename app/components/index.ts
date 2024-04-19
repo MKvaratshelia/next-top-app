9e4be743e7ac746e1bc33d55bf6492674bca37af
MKvaratshelia <kvaratsheliamihail@yandex.ru>
2024-04-19 16:54:54 +0300

    add svgr, Button, P

---
+++ app/components/index.ts
@@ -0,0 +1,3 @@
+export { P } from './P/P';
+export { Button } from './Button/Button';
+export { Htag } from './Htag/Htag';
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
 import Image from 'next/image';
 import styles from './page.module.scss';
-import { Htag } from './components/Htag/Htag';
+import { Button, Htag } from '@/components';
+import { P } from '@/components';
 
 export default function Home() {
     return (
         <main className={styles.main}>
             <Htag tag='h1'>Заголовок</Htag>
+            <Button appearance='primary' arrow='right'>
+                click
+            </Button>
+            <P size='l'>hello</P>
         </main>
     );
 }
